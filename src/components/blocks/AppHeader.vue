30e3380ffdace6f5dfd211770f3224a6be9471e2
Domagoj Svjetlicic <domagoj.svjetlicic@gmail.com>
2020-07-18 19:41:39 +0200

    add header lang switch buttons

--- src/components/blocks/AppHeader.vue
+++ src/components/blocks/AppHeader.vue
@@ -1,8 +1,16 @@
 <template>
   <header class="co-app-header">
-    <AppButton>
-      <span class="u-4">HR</span>
-    </AppButton>
+    <div class="left">
+
+    </div>
+    <div class="right">
+      <AppButton class="lang-button">
+        <span class="u-5">EN</span>
+      </AppButton>
+      <AppButton class="lang-button">
+        <span class="u-5">HR</span>
+      </AppButton>
+    </div>
   </header>
 </template>
 
@@ -10,8 +18,9 @@
   import Vue from 'vue';
   import { Component } from 'vue-property-decorator';
   import AppButton from '@/components/elements/AppButton.vue';
+
   @Component({
-    components: { AppButton }
+    components: { AppButton },
   })
   export default class AppHeader extends Vue {
 
@@ -20,11 +29,31 @@
 
 <style lang="scss" scoped>
   .co-app-header {
+    display: flex;
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     background-color: $main;
-    padding: 5px;
+    padding: 5px 0;
+
+    .left, .right {
+      flex: 1;
+    }
+
+    .right {
+      text-align: right;
+      padding-right: percentage(2 / 24);
+    }
+
+    .left {
+      padding-left: percentage(5 / 24);
+    }
+
+    .lang-button {
+      &:first-of-type {
+        margin-right: 10px;
+      }
+    }
   }
 </style>
--- src/components/elements/AppButton.vue
+++ src/components/elements/AppButton.vue
@@ -16,10 +16,20 @@
 
 <style lang="scss" scoped>
   .co-app-button {
-    border: 1px solid $white;
+    border: 1px solid transparent;
     border-radius: 3px;
-    padding: 5px 10px;
+    padding: 5px 20px;
     background-color: $main;
     color: $white;
+    @include transition-transform;
+
+    &:hover {
+      cursor: pointer;
+      transform: scale(1.05);
+    }
+
+    &.is-active {
+      border: 1px solid $white;
+    }
   }
 </style>
